4080244f36fc933dc5654af8ad9b93e71db541f6
djcurtin <djcurtin@gmail.com>
2020-04-08 09:28:28 -0400

    Rewrote subnet.py
    Uses cleaner code and better comments

--- subnet.py
+++ subnet.py
@@ -1,5 +1,17 @@
+"""
+Script to determine characteristics of subnet and IP based on supplied CIDR notation.
+
+Takes input in x.x.x.x/y format where x.x.x.x is valid IP and y is 1-32 CIDR notation.
+From this input, script determines the following:
+    - Upstream Network ID: Network ID of network being subnetted.
+      - e.g., 1.2.3.4/24 = 1.2.3.0; 1.2.3.4/19 = 1.2.0.0
+    - Subnet mask in x.x.x.x format
+    - Total number of subnets created, and total number of available ip addresses
+    - All valid subnet network IDs and broadcast IDs created by supplied CIDR
+    - Subnet network ID and broadcast ID for supplied IP
+    - Whether supplied IP is valid IP for calculated subnet
+"""
 import sys
-import math
 
 #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 #: Error handling functions
@@ -9,7 +21,7 @@ def show_help():
     Script is designed to take a properly formatted CIDR notation and calculate subnet information.
     After displaying subnet information, script will determine whether IP is valid host IP for the subnet.
 
-    Usage: subnet.py [ip/cidr] where ip is in ipv4 x.x.x.x format and cidr is between 1 and 31
+    Usage: subnet.py [ip/cidr] where ip is in ipv4 x.x.x.x format and cidr is between 1 and 32
     Note:  the slash (/) must immediately follow the ip. NO SPACES IN THE NOTATION.
 
     example: subnet.py 172.16.160.0/19
@@ -64,232 +76,175 @@ if len(ip) != 4:
     ip_octet_error()
     exit()
 
-if cidr < 1 or cidr > 31:
+if cidr < 1 or cidr > 32:
     cidr_error()
+    exit()
+
+#: CIDR of /32 means the IP address itself. Nothing more to be done
+if cidr == 32:
+    ip = '.'.join([ str(s) for s in ip ])
+    print(f"\n{sys.argv[1]} means IP address {ip}.\n")
     exit()
 
 #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 #:::::::::::::::::::::: And Now The Main Event ::::::::::::::::::::::::::::::::
 #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-#: Use CIDR to determine host ID octet.
+#: First figure out what I call the first host octet of the IP address.
 #:
-##: Host ID octet is what I call the first octet of the subnet mask, which is 
-##: not 255. 
+##: This is what I call the first octet of the IP address where the
+##: corresponding subnet mask octet is not 255. This is easy to figure out. The
+##: formula is ( CIDR // 8 ) + 1. This will be used later.
 ##:
-##: /1 - /7 starts in octet 1
-##: /8 - /15 is 2, etc
+##: x // y is equivalent to rounding down a division to zero decimal places. We
+##: then add 1 to the answer. So (7 // 8) + 1 = 1, which means the first subnet
+##: mask octet is not 255, which is true. (9 // 8) + 1 = 2, which means the
+##: subnet mask is 255.x.0.0
 ##:
-##: Easily done by rounding up cidr / 8, and adding 1 if it lands on 8, 16, or 24
-##:
-host_octet = math.ceil( cidr / 8 )
-host_octet = host_octet if cidr % 8 != 0 else host_octet + 1
+host_octet = (cidr // 8) + 1
 
-#: Build subnet mask using binary
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#: Calculating subnet mask
 #:
-##: CIDR is the number of 1s in the subnet mask, which will always be filled from
-##: left-to-right. So /1 is "1000000...", /2 is "1100000...." and so on.
+##: Subnet mask is simple: a string of 32 ones or zeros, which begins with 
+##: CIDR consecutive ones. So /1 means 10000..., /8 111111110000...
+##: We break this into an array of 4 octets so it's easier to work with.
 ##:
-##: This makes use of pythons string multiple functionality to make the binary
-##: subnet mask, which is then turned into an array of decimal values.
+##: e.g.: /19 subnet mask is 255.255.224.0, which this will store as 
+##:       [255, 255, 224, 0]
 ##:
-subnet_mask = ( "1" * cidr ) + ( "0" * (32 - cidr) )
-subnet_mask = [ int(subnet_mask[i:i+8], 2) for i in [0, 8, 16, 24] ]
+subnet_bits = ("1" * cidr) + ("0" * (32 - cidr))
+subnet_mask = [ int(subnet_bits[i : i+8], 2) for i in range(0, 32, 8) ]
 
-#: Find upstream Network ID. This is the network ID being subnetted
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#: Calculate number of total ip addresses
 #:
-##: Upstream id for a 172.16.160.x/24 CIDR will be 172.16.160.0.
-##: For 172.16.160.x/19, however, host id begins in octet 3 so upstream is 172.16.0.0.
+##: Total ip addresses available by particular CIDR. Quickest way to do this
+##: is to take total number of IP addresses available in IPv4 (2 ^ 32) and
+##: divide is by 2 ^ CIDR. Easiest way to do this is to subtract the powers,
+##: which gives us 2 ^ (32 - CIDR).
 ##:
-##: The code below takes the first x octets of the submitted ip, where x is the number of
-##: octets of the subnet mask equaliing 255. It then adds zeroes to the end.
+##: Note: This is total IP addresses, not total host addresses per subnet.
+##:       Because each subnet needs one network id and one broadcast id, the
+##:       number of host IPs per subnet is total IPs minus 2.
 ##:
-##: upstream_id will be an array of decimal values, to make it easier to work with
-##:
-upstream_id = ip[0:host_octet-1] + [ 0 ] * ( 5 - host_octet )
+total_ip_addresses = 2 ** (32 - cidr)
 
-# Calculate number of networks and hosts created by subnet mask in the host bit portion
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#: Calculate number of subnets created
 #:
-##: Number of subnets will be equal to 2^(cidr % 8). e.g., 2^(1 % 8) = 2, 2^(19 % 8) = 8
-##: Maximum hosts per octet is 256. So to get host count, divide 256 by # of subnets
+##: This is simple when you understand what CIDR and subnet mask are. For every
+##: bit in an octet, you double the amount of subnets created, which is to say
+##: 2 ^ (bits), where bits is going to be equal to CIDR % 8.
 ##:
-subnets = 2 ** ( cidr % 8 )
-hosts   = int( 256 / subnets )
+##: x % y returns the remainder of x/y. So 1 % 8 = 1, 9 % 8 = 1, 8 % 8 = 0, etc
+##:
+##: examples:
+##:   /1 makes the subnet mask of octet 1 "10000000". This creates 2 subnets
+##:   /2 is "11000000" which creates 4 subnets [00, 01, 10 and 11]
+##:   /3 is "11100000" which creates 8, and so on
+##:   /9 does what /1 does, but for octet 2, /10 same as /2 and so on.
+##:
+##: So number of subnets is 2 ^ CIDR for CIDR between 1 and 7, but after 7,
+##: it loops back around to 1 because /8, /16 and /24 creates one subnet. Thus,
+##: we use mod (%) operator. Number of subnets, thus, is as follows:
+##:
+subnets = 2 ** (cidr % 8)
 
-#: Now calculate the network IDs, and broadcast IDs for all subnets under upstream_id/CIDR
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#: Get upstream network ID
 #:
-##: First, we store the non-host values in the upstream_id list
+##: This is the network ID of the network being subnetted. In other words, it
+##: is every octet in the IP address which corresponds to a 255 octet subnet
+##: mask, and zeros for the rest. Here we just take the first host_octet octets
+##: and do not add zeros to make it easier to work with below. We add the zeros
+##: when outputting it.
 ##:
-network_octets = upstream_id[0:host_octet-1]
+upstream_id = ip[0 : host_octet-1]
 
-#: This calculates the first and last values in each subnet's range for the first non-network octet
-#: which will be used to calculate the subnet network IDs and broadcast IPs
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#: Calculate all valid subnet network IDs and broadcast IDs
 #:
-##: The number of hosts per subnet dictates the ranges here. So on a /25, for instance,
-##: there are 128 host addresses (0 to 127 and 128 to 255). And on a /26 there are 64,
-##: (0 to 63, 64 to 127, 128 to 191, 192 to 255). Each time you add a bit to the CIDR,
-##: you double the number of subnets, and halve the number of hosts per subnet.
+##: Each subnet will make up n ip addresses. So calculating all the subnet
+##: network IDs is easily done by starting with upstream_id and increasing the
+##: host octet by (hosts_per_subnet), starting at zero. Thus, on a /27, which
+##: creates subnets with 32 ip addresses each, we should see the following
+##: values: 0, 32, 64, 96, 128, 160, 192, 224. And, indeed, these will be our
+##: network IDs.
 ##:
-##: e.g., for 192.168.1.0/17, the first non-network octet is where the 1 is located. But
-##: the lowest value here is 0 so the subnets are:
-##: 192.168.0 to 192.168.127, and
-##: 192.168.128 to 192.168.255
+##: The broadcast ID, since it is the all-ones version, will be the highest
+##: possible value, so using the same /27 with the network values above, our
+##: broadcast values will be the highest end, which is as follows:
+##: [31, 63, 95, 127, 159, 191, 223, 255]
 ##:
-##: for 192.168.1.0/24 the first non-network octet is the last one. But the host octet does not
-##: contain subnet bits, so the range is 192.168.1.0 to 192.168.1.255
-##:
-##: for 192.168.1.0/25, the first is also the last octet, but now there is one subnet bit.
-##: Thus, the subnets here are:
-##: 192.168.1.0 to 192.168.1.127, and
-##: 192.168.1.128 to 192.168.1.255
-##:
-subnet_data = [ (hosts * i, hosts * (i + 1) - 1) for i in range( subnets ) ]
+id_per_subnet = 2 ** (8 - (cidr % 8))
 
-#: Make a list of all subnet network IDs.
-#: 
-##: This is done by combining the network_octets with the low end of each of the
-##: subnet_data entries calculated above, and filling remaining octets with zeros.
-##:
-subnet_list = [ network_octets + [s[0]] + ((4 - host_octet) * [0]) for s in subnet_data ]
+subnet_ids = []
+broadcasts = []
+subnet_index = -1
+step = 0
+for i in range(0, 256, id_per_subnet):
+    network_id = i
+    broadcast  = network_id + id_per_subnet - 1
 
-#: Determine broadcast IDs for each subnet.
+    #: Subnet network id is first address in subnet. All zeros after subnet mask
+    tmp_subnet = upstream_id + [network_id] + ([0] * (4 - host_octet))
+    subnet_ids.append( '.'.join([ str(s) for s in tmp_subnet ]) )
+
+    #: Broadcast is all ones so highest host value and all ones afterward
+    tmp_broadcast = upstream_id + [broadcast] + ([255] * (4 - host_octet))
+    broadcasts.append( '.'.join([ str(b) for b in tmp_broadcast ]) )
+
+    #: If supplied IP falls into this subnet, keep note of the index for later 
+    #: and validate IP by making sure it is not the network id or broadcast id
+    if (ip[host_octet-1] >= network_id) and (ip[host_octet-1] <= broadcast):
+        subnet_index = step
+
+    step = step + 1
+
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#: Output time.
 #:
-##: Broadcast IDs are like network IDs computed above, but using the high end of
-##: subnet_data entries, and filling remaining octets with 255
-##:
-broadcast_list = [ network_octets + [s[1]] + ((4 - host_octet) * [255]) for s in subnet_data ]
+#: First we format our data. Convert all ip lists into string dot format.
+ip_bits = '.'.join([ format(s, "08b") for s in ip ]) 
 
-#: Find subnet ID on which IP given at command line is located.
+ip_address  = '.'.join([ str(s) for s in ip ])
+subnet_mask = '.'.join([ str(s) for s in subnet_mask ])
+upstream_id = '.'.join([ str(s) for s in upstream_id + ([0] * (5 - host_octet)) ])
+ip_per_sub  = int( total_ip_addresses / subnets )
+
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#: Is IP Address a valid one for this subnet?
 #:
-##: Just search the subnet_data array until one where the host_octet is between
-##: the low end and the high end is found
-##: 
-i = 0
-subnet_id = -1
-host_id   = ip[ host_octet - 1]
-
-while (subnet_id == -1) and (i < subnets):
-    sub_low = subnet_data[i][0]
-    sub_hi  = subnet_data[i][1]
-
-    if (host_id >= sub_low) and (host_id <= sub_hi):
-        subnet_id = i
-
-    i = i + 1
-
-#: Validate IP supplied at command line
+#: Not if it matches either the network ID or broadcast ID. Yes otherwise
 #:
-##: The network ID and broadcast IP of a subnet are reserved, so these are not
-##: valid IP addresses on a subnet. So to validate the supplied IP, we simply
-##: make sure it is greater than the network ID and less than broadcast IP.
-##:
-##: Both subnet id and broadcast ip are created the same way:
-##: - Start with upstream network id (network_octets calculated above)
-##: - Add low end value of host id for subnet id and high end for broadcast ip
-##: - Fill any remaining octets with 0 for subnet id and 255 for broadcast ip
-##:
-##: e.g., for 172.16.160.0/19:
-##: - The upstream network is 172.16.0.0 so the network octets are 172.16
-##: - The subnet is between 160 and 191 so 160 for subnet, 191 for broadcast
-##:   - 172.16.160 [low end] to 172.16.191 [high end]
-##: - Add zeros to ensure four octets to get subnet id, and 255 for broadcast
-##:   - subnet id: 172.16.160.0
-##:   - broadcast: 172.16.191.255
-##:
-ip_subnet = network_octets + [sub_low] + ((4 - host_octet) * [0])
-ip_broadc = network_octets + [sub_hi] + ((4 - host_octet) * [255])
-
-#: Simplest way to validate IPs
-#:
-##: Loop through each octet and compare supplied IP with subnet id and broadcast
-##: IP. If it matches one of them exactly, IP is invalid on that subnet.
-##:
-is_netid = True
-is_bcid  = True
-
-for i in range(0, 4):
-    is_netid = is_netid and (ip_subnet[i] == ip[i])
-    is_bcid = is_bcid and (ip_broadc[i] == ip[i])
-
-#: Format our validity reporting string for output later
-#:
-if is_netid:
-    is_valid = "IP Address {} is an INVALID address on subnet {}.\nREASON: IP Address is Network ID."
-elif is_bcid:
-    is_valid = "IP Address {} is an INVALID address on subnet {}.\nREASON: IP Address is Broadcast Address."
+if ip_address == subnet_ids[ subnet_index ]:
+    is_valid = f"\nIP Address {ip_address}/{cidr} is an INVALID IP on subnet id {subnet_ids[ subnet_index]}.\nREASON: IP Address is the network ID for the associated subnet.\n"
+elif ip_address == broadcasts[ subnet_index ]:
+    is_valid = f"\nIP Address {ip_address}/{cidr} is an INVALID IP on subnet id {subnet_ids[ subnet_index]}.\nREASON: IP Address is the broadcast ID for the associated subnet.\n"
 else:
-    is_valid = "IP Address {} is a VALID address on subnet {}."
+    is_valid = f"IP Address {ip_address}/{cidr} is a VALID IP on subnet id {subnet_ids[ subnet_index]}.\n"
 
-#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-#: Output our results
-#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-#: To make the output pretty, each IP array is:
-#: 1) Converted from decimal values to strings
-#: 2) Combined into a single string, separated by periods (.)
-#:
-ip  = '.'.join([ str(i) for i in ip ])
-snm = '.'.join([ str(i) for i in subnet_mask ])
-ups = '.'.join([ str(i) for i in upstream_id ])
+subnet_info = '\n'.join([ subnet_ids[i] + "\t" + broadcasts[i] for i in range(0, subnets) ])
+subnet_bits = '.'.join([ subnet_bits[i : i+8] for i in [0, 8, 16, 24] ])
 
-#: Create the arrays which will store every subnet ID, and every broadcast
-#: IP possible for the given IP and CIDR.
-#:
-sns = []
-bcs = []
+output = f"""
+Calculating subnet values for:
 
-#: Run through the list of subnet ids and broadcast ips calculated above,
-#: convert them to strings and combine them into period separated strings
-#: like we did with ip, subnet mask and upstream network id above
-#:
-for s in range(subnets):
-    sns.append( str(s) + ")\t" + '.'.join( [str(i) for i in subnet_list[s]] ) )
-    bcs.append( str(s) + ")\t" + '.'.join( [str(i) for i in broadcast_list[s]] ) )
+IP Address:\t{ip_address}
+Subnet Mask:\t{subnet_mask}
+Host Octet:\t{host_octet}
+Upstream ID:\t{upstream_id}
 
-#: Also convert subnet ID for subnet to which supplied IP belongs
-#:
-ip_subnet = '.'.join([ str(i) for i in ip_subnet ])
+IP in binary:\t{ip_bits}
+Subnet binary:\t{subnet_bits}
 
-#: Host addresses per subnet using this CIDR
-#:
-##: Total number of hosts (assuming CIDR of /1) is (2^8)^4
-##: - 2^8 for 8 bits per byte in each IPv4 octet
-##: - Then raised to the fourth power because of 4 octets
-##:
-##: To calculate total hosts per subnet, you take that value
-##: and divide by 2 as many times as you add a bit to the subnet mask, which
-##: is the value of CIDR. This means divide by (2 ^ CIDR)
-##:
-##: The bitwise shift (x >> y) is the same as x / (2 ^ y)
-##:
-hosts = ((2 ** 8) ** 4) >> cidr
+Total IPs:\t{total_ip_addresses}
+Total Subnets:\t{subnets}
+IPs Per Subnet:\t{ip_per_sub}
 
-#: Sloppy way of making the output string, but it gets the job done, and made it
-##: easy to build the output as I went along. Just plug in the values calculated,
-##: and enjoy the results.
-##:
-output = """
-Host IP:\t{ip}
-Subnet Mask:\t{snm}
-Upstream:\t{ups}
-Hosts:\t\t{h}
-Subnets:\t{s}
+The following are the network IDs and broadcast IDs for each subnet:
+{subnet_info}
 
-Network IDs of each subnet:
-{sns}
-
-Broadcast IDs of each subnet:
-{bcs}
-
-{iv}
-""".format(
-        ip  = ip,
-        snm = snm,
-        ups = ups,
-        h   = hosts,
-        s   = subnets,
-        sns = "\n".join(sns),
-        bcs = "\n".join(bcs),
-        iv  = is_valid.format(ip, ip_subnet)
-)
+{is_valid}"""
 
 print(output)
